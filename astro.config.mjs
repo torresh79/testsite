7e3e1e7da3066d0f86f6394ef36a5307e9836cfe
unknown <hernant@knowtech.ws>
2025-11-06 09:05:35 -0300

    cambios por errores en le deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+	outDir: 'dist',
 	site: 'https://torresh79.github.io',
 	base: 'testsite',
 	vite: {
